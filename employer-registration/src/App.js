f28ee36a44bfda4a9d6192ee5ae7f4d52d095cb6
belkoudia fatima-ezzahra <fatima.ezzahra.belkoudia@gmail.com>
2025-03-24 10:35:45 +0100

    Refactor App.js: remove unused logo import and streamline React import

--- employer-registration/src/App.js
+++ employer-registration/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import React, {useState} from 'react'; 
+import React from 'react'; 
 import UserForm from './components/UserForm';
 import './App.css';
 import { ToastContainer } from "react-toastify";
