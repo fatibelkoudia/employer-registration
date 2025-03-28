be909fc7f8a4f7fd94c05680e577950ca97e6496
belkoudia fatima-ezzahra <fatima.ezzahra.belkoudia@gmail.com>
2025-03-28 10:15:49 +0100

    Refactor UserList component to display users in a table format and update styling

--- employer-registration/src/components/UserList.jsx
+++ employer-registration/src/components/UserList.jsx
@@ -3,26 +3,42 @@ import PropTypes from "prop-types";
 import "./UserListStyle.css";
 
 /**
- * UserList displays a list of registered users.
+ * UserList displays a list of registered users in a table.
  *
  * @component
  * @param {Object[]} users - Array of user objects.
- * @returns {JSX.Element} The rendered user list.
+ * @returns {JSX.Element} The rendered user table.
  */
 function UserList({ users }) {
   if (!users.length) return null;
 
   return (
     <div className="user-list">
-      <h3>Liste des inscrits :</h3>
-      <ul>
-        {users.map((user, index) => (
-          <li key={index}>
-            {user.firstName} {user.lastName} – {user.email} – {user.birthDate} –{" "}
-            {user.city} – {user.postalCode}
-          </li>
-        ))}
-      </ul>
+      <h3>Liste des inscrits</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>Email</th>
+            <th>Date de naissance</th>
+            <th>Ville</th>
+            <th>Code postal</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.email}</td>
+              <td>{user.birthDate}</td>
+              <td>{user.city}</td>
+              <td>{user.postalCode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
